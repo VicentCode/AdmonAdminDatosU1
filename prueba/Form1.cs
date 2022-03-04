598505bbb5f729c40f476adb7e3761464b372652
vicen <vicentcode@gmail.com>
2022-03-03 22:11:55 -0600

    Add project files.

---
+++ prueba/Form1.cs
@@ -0,0 +1,317 @@
+namespace prueba
+{
+    public partial class Form1 : Form
+    {
+        int lineGuide = 0;
+        String[] lineAlm = File.ReadAllLines(@"C:\Users\vicen\OneDrive\Escritorio\Datos.txt");
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream conector = new FileStream(@"C:\Users\vicen\OneDrive\Escritorio\Datos.txt", FileMode.Append);
+            StreamWriter canalEscritura = new StreamWriter(conector);
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Ingrese un nombre");
+                canalEscritura.Close();
+
+            }
+            else if (textBox2.Text == "")
+            {
+                MessageBox.Show("Ingrese una carrera");
+                canalEscritura.Close();
+
+            }
+            else if (textBox3.Text == "")
+            {
+                MessageBox.Show("Ingrese un numero telefonico");
+                canalEscritura.Close();
+
+            }
+            else if (textBox4.Text == "")
+            {
+                MessageBox.Show("Ingrese una dirrecion");
+                canalEscritura.Close();
+
+            }
+            else
+            {
+                canalEscritura.WriteLine(textBox1.Text);
+                canalEscritura.WriteLine(textBox2.Text);
+                canalEscritura.WriteLine(textBox3.Text);
+                canalEscritura.WriteLine(textBox4.Text);
+
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                lineGuide = 0;
+
+                //Se cierra el archivo
+                canalEscritura.Close();
+            }
+        }
+
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string[] lineas = File.ReadAllLines(@"C:\Users\vicen\OneDrive\Escritorio\Datos.txt");
+
+
+            if (lineas.Length == 0)
+            {
+                MessageBox.Show("No hay registros almacenados :(");
+            }
+            else
+            {
+
+                if (lineGuide >= lineas.Length)
+                {
+                    lineGuide = 0;
+                    textBox1.Text = lineas[lineGuide];
+                    textBox2.Text = lineas[lineGuide + 1];
+                    textBox3.Text = lineas[lineGuide + 2];
+                    textBox4.Text = lineas[lineGuide + 3];
+                    lineGuide = lineGuide + 4;
+                }
+                else
+                {
+                    textBox1.Text = lineas[lineGuide];
+                    textBox2.Text = lineas[lineGuide + 1];
+                    textBox3.Text = lineas[lineGuide + 2];
+                    textBox4.Text = lineas[lineGuide + 3];
+                    lineGuide += 4;
+                }
+            }
+
+
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+
+        {
+            int cont = 0;
+            int cantidad;
+            FileStream conector = new FileStream(@"C:\Users\vicen\OneDrive\Escritorio\Datos.txt", FileMode.Open);
+            StreamReader lector = new StreamReader(conector);
+            while (lector.EndOfStream == false)
+            {
+                lector.ReadLine();
+                cont++;
+            }
+            cantidad = cont / 4;
+            MessageBox.Show("el archvio tiene " + cantidad + " registros");
+            lector.Close();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            lineGuide = 0;
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            FileStream conector = new FileStream(@"C:\Users\vicen\OneDrive\Escritorio\Datos.txt", FileMode.Open);
+            StreamReader lector = new StreamReader(conector);
+
+            while (lector.EndOfStream == false)
+            {
+                if (lector.ReadLine() == textBox1.Text)
+                {
+                    textBox2.Text = lector.ReadLine();
+                    textBox3.Text = lector.ReadLine();
+                    textBox4.Text = lector.ReadLine();
+                }
+                else
+                {
+                    lector.ReadLine();
+                    lector.ReadLine();
+                    lector.ReadLine();
+
+                }
+            }
+            lector.Close();
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            lineAlm = File.ReadAllLines(@"C:\Users\vicen\OneDrive\Escritorio\Datos.txt");
+            FileStream conector = new FileStream(@"C:\Users\vicen\OneDrive\Escritorio\Datos.txt", FileMode.Create);
+            StreamWriter escritor = new StreamWriter(conector);
+
+
+
+
+
+            if (MessageBox.Show("¿Desea guardar los cambios?", "Prueba",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+
+                for (int i = 0; i < lineAlm.Length; i++)
+                {
+                    if (lineAlm[i] == textBox1.Text)
+                    {
+                        i++;
+                        textBox2.Text = lineAlm[i];
+                        i++;
+                        textBox3.Text = lineAlm[i];
+                        i++;
+                        textBox4.Text = lineAlm[i];
+                    }
+
+                }
+
+                for (int i = 0; i < lineAlm.Length; i++)
+                {
+                    escritor.WriteLine(lineAlm[i]);
+                }
+                escritor.Close();
+            }
+            else
+            {
+                for (int i = 0; i < lineAlm.Length; i++)
+                {
+                    if (lineAlm[i] == textBox1.Text)
+                    {
+                        i++;
+                        lineAlm[i] = textBox2.Text;
+                        i++;
+                        lineAlm[i] = textBox3.Text;
+                        i++;
+                        lineAlm[i] = textBox4.Text;
+                    }
+                }
+
+                for (int i = 0; i < lineAlm.Length; i++)
+                {
+                    escritor.WriteLine(lineAlm[i]);
+                }
+                escritor.Close();
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            lineAlm = File.ReadAllLines(@"C:\Users\vicen\OneDrive\Escritorio\Datos.txt");
+
+            FileStream conector = new FileStream(@"C:\Users\vicen\OneDrive\Escritorio\Datos.txt", FileMode.Create);
+            StreamWriter escritor = new StreamWriter(conector);
+            if (textBox1.Text != "")
+            {
+
+                if (MessageBox.Show("¿Desea eliminar el registro de " + textBox1.Text + "?", "Confirmacion",
+                   MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    for (int i = 0; i < lineAlm.Length; i++)
+                    {
+                        if (lineAlm[i] == textBox1.Text)
+                        {
+
+                            lineAlm[i] = "null";
+                            i++;
+                            lineAlm[i] = "null";
+                            i++;
+                            lineAlm[i] = "null";
+                            i++;
+                            lineAlm[i] = "null";
+
+                            break;
+
+                        }
+                        else
+                        {
+                            i = i + 3;
+                        }
+                    }
+                    for (int i = 0; i < lineAlm.Length; i++)
+                    {
+                        if (lineAlm[i] != "null")
+                        {
+                            escritor.WriteLine(lineAlm[i]);
+
+                        }
+                    }
+                    escritor.Close();
+
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                    textBox3.Text = "";
+                    textBox4.Text = "";
+                }
+                else
+                {
+                    escritor.Close();
+                    FileStream conectorr = new FileStream(@"C:\Users\vicen\OneDrive\Escritorio\Datos.txt", FileMode.Open);
+                    StreamWriter escritorr = new StreamWriter(conectorr);
+                    for (int i = 0; i < lineAlm.Length; i++)
+                    {
+                        escritorr.WriteLine(lineAlm[i]);
+                    }
+                    escritorr.Close();
+                }
+            }
+            else
+            {
+                escritor.Close();
+                FileStream conectorr = new FileStream(@"C:\Users\vicen\OneDrive\Escritorio\Datos.txt", FileMode.Open);
+                StreamWriter escritorr = new StreamWriter(conectorr);
+                for (int i = 0; i < lineAlm.Length; i++)
+                {
+                    escritorr.WriteLine(lineAlm[i]);
+                }
+
+                MessageBox.Show("Primero seleccione un elemento del registro");
+                escritorr.Close();
+
+            }
+        }
+    }
+}
